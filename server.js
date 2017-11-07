df5a362f1ac11fea5c6dcd4949a88aaa050620e8
Susu LA <lahgazi.safae@gmail.com>
2017-11-07 08:48:23 -0500

    modified server.js

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const accountSid = 'AC4305d609ba68cee1ce878ce0a075e142';
 const authToken = '29171a6dbca89c29128d78c93f6f38a6';
 const client = require('twilio')(accountSid, authToken);
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
-const twiml = new MessagingResponse();
 
 var express = require('express');
 var app = express();
@@ -16,15 +15,16 @@ app.use(bodyparser.urlencoded({extended: true}));
 //changed port for localhost:3000 
 app.set('port', (process.env.PORT || 3000));
 
-app.use('/', function(request, response){
-	//console.log('this console message works');
-	response.send("Twilio with Express Program!");
-});
+// app.use('/', function(request, response){
+// 	//console.log('this console message works');
+// 	 response.send();
+// });
 
 app.post('/sms', function(req, res) {
-	console.log('post request was made');
 
-  twiml.message('Hi, Culpability has recieved your non-emergency input. This is the unique identifying # associated with this input: XXXX');
+const twiml = new MessagingResponse();
+
+  twiml.message('testing 123');
 
   res.writeHead(200, {'Content-Type': 'text/xml'});
   res.end(twiml.toString());
@@ -32,5 +32,5 @@ app.post('/sms', function(req, res) {
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+  // console.log('Node app is running on port', app.get('port'));
 });
